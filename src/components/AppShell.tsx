e06c3778511cd60d6df138e2f83c0420b4125a17
root <root@srv1475101.hstgr.cloud>
2026-04-02 19:24:11 +0000

    fix: layout overflow

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -4,9 +4,9 @@ import Sidebar from './Sidebar'
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full">
+    <div className="min-h-screen">
       <Sidebar />
-      <main className="ml-64 flex-1 min-h-screen bg-gray-50 p-8">
+      <main className="ml-64 p-8">
         {children}
       </main>
     </div>
